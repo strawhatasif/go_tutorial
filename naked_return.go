4eb1d9c90f2ca72a7b4bef0e8b32958233528d71
Asif A Fasih <aafasih@gmail.com>
2021-07-25 18:07:28 -0500

    small adjustment to show that we are manipulating naked return properly

--- naked_return.go
+++ naked_return.go
@@ -9,18 +9,19 @@ import (
 
 /*
 First, we use the Split function to split on the space delimiter
-Then, we assign the first token to the first name variable, and second token to the last name variable
+Then, we assign the first token to the last name variable, and second token to the first name variable
 */
 func divideFirstAndLastName(fullName string) (first_name, last_name string) {
 	name := strings.Split(fullName, " ")
 
-	first_name = name[0]
-	last_name = name[1]
+	last_name = name[0]
+	first_name = name[1]
 
 	//notice, we are not specifiying what arguments to return
 	return
 }
 
 func main() {
-	fmt.Println(divideFirstAndLastName("Rachel Pepperstaffere"))
+	//whoops! we are passing the last name and first name in the wrong order!
+	fmt.Println(divideFirstAndLastName("Pepperstaffere Rachel"))
 }
